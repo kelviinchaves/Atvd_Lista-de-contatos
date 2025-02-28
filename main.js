d406098f717a6517747f1030f25d65a9392ca829
kelvin <eng.kelvin@outlook.com>
2025-02-27 23:24:15 -0300

    Atividade; cadastro de contatos e nome

---
+++ main.js
@@ -0,0 +1,41 @@
+const form = document.getElementById('form-dados');
+const tel = [];
+const nome = [];
+let linhas = '';
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+
+    adicionaLinha();
+    atualizaTabela();
+
+});
+
+function adicionaLinha() {
+    const inputNome = document.getElementById('nome');
+    const inputTelefone = document.getElementById('telefone');
+
+    if (nome.includes(inputNome.value)) {
+        alert(`O nome ${inputNome.value} já foi inserido`);
+    } else {
+
+        nome.push(nome.value);
+        tel.push(tel.value);
+
+        linhas +=
+            `<tr>
+            <td>${inputNome.value}</td>
+            <td>${inputTelefone.value}</td>
+        </tr>` ;
+
+    }
+
+    inputNome.value = '';
+    inputTelefone.value = '';
+
+}
+
+function atualizaTabela() {
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+}
